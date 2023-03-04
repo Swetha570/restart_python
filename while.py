fa21d1c0b3e11866bc35b5cda7012f9d3c7aab29
Swetha570 <beereddyswetha91@gmail.com>
2023-03-04 22:05:09 +0100

    Encryption and decryption

---
+++ while.py
@@ -0,0 +1,8 @@
+text= input("I love learning python")
+words = list(set(text.split()))
+while text == str(type(myString)):
+    print(text[0])
+    print(test[1])
+    print(test[2])
+    print(test[3])
+print(str(type(myString)))
