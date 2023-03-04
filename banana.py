4cd8d01c386d94bfec9f3efc8c5be0de0ad784a8
Swetha570 <beereddyswetha91@gmail.com>
2023-03-04 22:04:51 +0100

    Encryption and decryption

---
+++ banana.py
@@ -0,0 +1,8 @@
+x=input("printow many bananas you have")
+banana = int(x)
+if banana >= 5:
+  print("I have bunch of bananas")
+elif banana >= 4:
+  print("I have small bunch of bananas")
+else :
+  print("i have no bananas")
