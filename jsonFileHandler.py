e6e4b653a473c0de9a6fe30f0c554c1b7393995e
Swetha570 <beereddyswetha91@gmail.com>
2023-03-06 11:16:49 +0100

    File Handlers and Module forRetrieving Information

---
+++ jsonFileHandler.py
@@ -0,0 +1,16 @@
+import json
+def readJsonFile(fileName):
+    data=""
+    def readJsonFile(fileName):
+        data = ""
+    with open('Files/insulin.json') as json_file:
+        data = json.load(json_file)
+    return data
+def readJsonFile(fileName):
+    data = ""
+    try:
+        with open(fileName) as json_file:
+            data = json.load(json_file)
+    except IOError:
+        print("Could not read file")
+    return data
